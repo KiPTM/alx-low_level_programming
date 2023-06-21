27ecdb6bb2d7005b7ed3585e2c31fe8767d022ba
mathew <mathewtuwei@gmail.com>
2023-06-20 23:23:08 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- * main.h -entry point of the programme
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
  * Written By Tuwei
