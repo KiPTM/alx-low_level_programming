7e220e01c376d66372c1db78ac876d0f80b32d04
KiPTM <mathewtuwei@gmail.com>
2023-12-11 10:26:04 -0800

    gft

---
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -0,0 +1,29 @@
+// libdynamic.c
+
+#include <stdio.h>
+
+int _putchar(char c) {
+    // Implementation of _putchar function
+    // Add your implementation here
+    return 0;
+}
+
+int _islower(int c) {
+    // Implementation of _islower function
+    // Add your implementation here
+    return 0;
+}
+
+int _isalpha(int c) {
+    // Implementation of _isalpha function
+    // Add your implementation here
+    return 0;
+}
+
+// ... Implementations of other listed functions ...
+
+char *_strstr(char *haystack, char *needle) {
+    // Implementation of _strstr function
+    // Add your implementation here
+    return NULL;
+}
